0191d3a5b486885d6121242d24104bcc3657dea1
Sean.Mars <mars0222@gmail.com>
2016-07-04 00:11:54 +0800

    - Update test process;

--- test/test.js
+++ test/test.js
@@ -1,11 +1,7 @@
 var path = require('path');
-var jsonfile = require('jsonfile');
 var e2j = require('../excel2json.js');
 
 var fp = path.resolve('./test/data/test.xlsx');
-// var d = e2j.parse(fp, 'test');
-// console.log(JSON.stringify(d, null, 4));
-
 var dd = e2j.parse(fp, 'demo');
 console.log(JSON.stringify(dd, null, 4));
 
