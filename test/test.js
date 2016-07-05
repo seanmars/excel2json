e2f0b6a37407c4667b4e82a97678ac5b7d8c54b6
Sean.Mars <mars0222@gmail.com>
2016-07-06 03:18:58 +0800

    - Update the test;

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
 var path = require('path');
 var e2j = require('../excel2json.js');
 
-var fp = path.resolve('./test/data/test.xlsx');
-var dd = e2j.parse(fp, 'demo');
-console.log(JSON.stringify(dd, null, 4));
+e2j.load('./test/data/template.json', function(err, jsonObj) {
+    console.log(jsonObj);
+    var fp = path.resolve('./test/data/test.xlsx');
+    var dd = e2j.parse(fp, 'demo', jsonObj);
+    console.log(JSON.stringify(dd, null, 4));
 
-// TODO check the dir is exists.
-e2j.save('./test/output/test.json', dd);
+    fs.mkdirSync('./test/output/', 755);
+    e2j.save('./test/output/test.json', dd);
+});
