9c1fb240f2420466943905ea0bd2d3711e6ff3d4
Sean.Mars <mars0222@gmail.com>
2016-07-08 10:13:04 +0800

    - Fixed the expection "Error: EEXIST: file already exists, mkdir", now will check the folder is exist or not;

--- test/test.js
+++ test/test.js
@@ -3,11 +3,16 @@ var path = require('path');
 var e2j = require('../excel2json.js');
 
 e2j.load('./test/data/template.json', function(err, jsonObj) {
+    const outputPath = './test/output';
+    var stats = fs.statSync(outputPath);
+    if (!stats.isDirectory()) {
+        fs.mkdirSync('./test/output/', 755);
+    }
+
     console.log(jsonObj);
     var fp = path.resolve('./test/data/test.xlsx');
+
     var dd = e2j.parse(fp, 'demo', jsonObj);
     console.log(JSON.stringify(dd, null, 4));
-
-    fs.mkdirSync('./test/output/', 755);
-    e2j.save('./test/output/test.json', dd);
+    e2j.save('./test/output/test_with_template.json', dd);
 });
