c6b6e65b38de58969a77068493699359846ec0a5
Sean.Mars <mars0222@gmail.com>
2016-07-31 12:20:40 +0800

    - add mocha.opts

--- test/test.js
+++ test/test.js
@@ -9,37 +9,47 @@ describe('Load file', function () {
     describe('#loadSheet()', function () {
         describe('when type is xlsx', function () {
             describe('when file is NOT exist', function () {
-                it('should throw expect', function () {
+                it('should throw expect', function (done) {
                     var filePath = './test/data/none.xlsx';
                     should.throws(() => e2jt.loadSheet(filePath, 'test'));
+
+                    return done();
                 });
             });
 
             describe('when sheet is exist', function () {
-                it('should return undefined', function () {
+                it('should return undefined', function (done) {
                     var sheet = e2jt.loadSheet('./test/data/test.xlsx', 'none');
                     should(sheet).not.ok();
+
+                    return done();
                 });
             });
 
             describe('when sheet is NOT exist', function () {
-                it('should return sheet object', function () {
+                it('should return sheet object', function (done) {
                     var sheet = e2jt.loadSheet('./test/data/test.xlsx', 'test');
                     should(sheet).ok();
+
+                    return done();
                 });
             });
 
             describe('without parameter [filePath] and [sheetName]', function () {
-                it('should return undefined', function () {
+                it('should return undefined', function (done) {
                     var sheet = e2jt.loadSheet();
                     should(sheet).not.ok();
+
+                    return done();
                 });
             });
 
             describe('without parameter [sheetName]', function () {
-                it('should return undefined', function () {
+                it('should return undefined', function (done) {
                     var sheet = e2jt.loadSheet('./test/data/test.xlsx');
                     should(sheet).not.ok();
+
+                    return done();
                 });
             });
         });
