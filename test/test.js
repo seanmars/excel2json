be211fc62093e79084601b861a0f92afd61631c2
Sean.Mars <mars@tizsoft.com>
2016-09-05 17:35:13 +0800

    Add more test case;

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('#loadSheet()', function () {
                 var filePath = './test/data/none.xlsx';
                 should.throws(() => e2jt.loadSheet(filePath, 'test'));
 
-                return done();
+                done();
             });
         });
 
@@ -20,7 +20,7 @@ describe('#loadSheet()', function () {
                 var sheet = e2jt.loadSheet('./test/data/test.xlsx', 'none');
                 should(sheet).not.ok();
 
-                return done();
+                done();
             });
         });
 
@@ -29,7 +29,7 @@ describe('#loadSheet()', function () {
                 var sheet = e2jt.loadSheet('./test/data/test.xlsx', 'test');
                 should(sheet).ok();
 
-                return done();
+                done();
             });
         });
 
@@ -38,7 +38,7 @@ describe('#loadSheet()', function () {
                 var sheet = e2jt.loadSheet();
                 should(sheet).not.ok();
 
-                return done();
+                done();
             });
         });
 
@@ -47,7 +47,7 @@ describe('#loadSheet()', function () {
                 var sheet = e2jt.loadSheet('./test/data/test.xlsx');
                 should(sheet).not.ok();
 
-                return done();
+                done();
             });
         });
     });
@@ -60,28 +60,35 @@ describe('#loadTemplate', function () {
             should(err).be.a.null();
             should(jsonObj).not.be.a.null();
 
-            // console.log(jsonObj);
-            jsonObj.should.have.properties('key');
-            jsonObj.should.have.properties('attributes');
-            jsonObj.should.have.properties('array');
-            jsonObj.should.have.properties('price');
+            should(jsonObj).have.properties('key');
+            should(jsonObj).have.properties('attributes');
+            should(jsonObj).have.properties('array');
+            should(jsonObj).have.properties('price');
 
-            jsonObj.key.should.be.eql('id');
+            should(jsonObj.key).be.eql('id');
 
-            jsonObj.attributes.should.be.a.Object();
-            jsonObj.attributes.value.should.be.eql('value');
-            jsonObj.attributes.name.should.be.eql('name');
+            should(jsonObj.attributes).be.Object();
+            should(jsonObj.attributes.value).be.eql('value');
+            should(jsonObj.attributes.name).be.eql('name');
+            should(jsonObj.attributes.array).be.Array();
+            should(jsonObj.attributes.array[0]).be.eql('value');
+            should(jsonObj.attributes.array[1]).be.eql('name');
 
-            jsonObj.array.should.be.a.Array();
-            jsonObj.array[0].should.be.eql('value');
-            jsonObj.array[1].should.be.eql('name');
-            jsonObj.array[2].should.be.a.Object();
-            jsonObj.array[2].value.should.be.eql('value');
-            jsonObj.array[2].name.should.be.eql('name');
+            should(jsonObj.array).be.Array();
+            should(jsonObj.array[0]).be.eql('value');
+            should(jsonObj.array[1]).be.eql('name');
 
-            jsonObj.price.should.be.eql('value');
+            should(jsonObj.array[2]).be.Object();
+            should(jsonObj.array[2].value).be.eql('value');
+            should(jsonObj.array[2].name).be.eql('name');
 
-            return done();
+            should(jsonObj.array[3]).be.Array();
+            should(jsonObj.array[3][0]).be.eql('value');
+            should(jsonObj.array[3][1]).be.eql('name');
+
+            should(jsonObj.price).be.eql('value');
+
+            done();
         });
     });
 });
@@ -89,14 +96,52 @@ describe('#loadTemplate', function () {
 // #parse()
 describe('#parse()', function () {
     it('should return right JSON Object', function (done) {
-        throw new exception('not completed.');
+        e2jt.loadTemplate('./test/data/template.json', function (err, jsonObj) {
+            if (err) {
+                done(err);
+            } else {
+                var data = e2jt.parse('./test/data/test.xlsx', 'test', jsonObj);
+                // console.log(JSON.stringify(data, '', 4));
+                should(data).be.Object();
+                should(data.version).be.eql('1.0.0');
+                should(data.name).be.eql('newname');
+
+                should(data.datas).be.Array();
+                should(data.datas.length).be.eql(7);
+                for (var i = 0; i < data.datas.length; i++) {
+                    var v = i + 1;
+                    should(data.datas[i].key).be.eql('Id_' + v);
+
+                    should(data.datas[i].attributes).be.Object();
+                    should(data.datas[i].attributes.value).be.eql('Value_' + v);
+
+                    should(data.datas[i].attributes.array).be.Array();
+                    should(data.datas[i].attributes.array[0]).be.eql(data.datas[i].attributes.value);
+                    should(data.datas[i].attributes.array[1]).be.eql(data.datas[i].attributes.name);
+
+                    should(data.datas[i].array).be.Array();
+                    should(data.datas[i].array[0]).be.eql(data.datas[i].attributes.value);
+                    should(data.datas[i].array[1]).be.eql(data.datas[i].attributes.name);
+
+                    should(data.datas[i].array[2]).be.Object();
+                    should(data.datas[i].array[2].value).be.eql(data.datas[i].attributes.value);
+                    should(data.datas[i].array[2].name).be.eql(data.datas[i].attributes.name);
+
+                    should(data.datas[i].array[3]).be.Array();
+                    should(data.datas[i].array[3][0]).be.eql(data.datas[i].attributes.value);
+                    should(data.datas[i].array[3][1]).be.eql(data.datas[i].attributes.name);
+                }
+
+                done();
+            }
+        });
     });
 });
 
 
 // #save()
-describe('#save()', function () {
-    it('should save right JSON Object', function (done) {
-        throw new exception('not completed.');
-    });
-});
+// describe('#save()', function () {
+//     it('should save right JSON Object', function (done) {
+//         done();
+//     });
+// });
